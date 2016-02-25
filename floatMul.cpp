d7b8e89131a0e761066cf3f4389d045d54f65ae0
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-02-25 18:27:13 +0900

    complete the ARM version   * add multiply implementation using NEON instructions   * add option to suppress warnings

--- floatMul.cpp
+++ floatMul.cpp
@@ -6,8 +6,18 @@
 #endif
 #include "floatMul.h"
 
-// requires AVX
 void float2half(float* src, short* dst, int length) {
+#if defined(__arm__) || defined(_M_ARM)
+	// requires NEON
+	const unsigned int cParallel = 4;
+	for (int i = 0; i <= length-cParallel; i+=cParallel)
+	{
+		float32x4_t float_vector = *(float32x4_t*)(src + i);
+		float16x4_t half_vector = vcvt_f16_f32( float_vector );
+		*(float16x4_t*)(dst + i) = half_vector;
+	}
+#elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
+	// requires AVX
 	const unsigned int cParallel = 8;
 	for (int i = 0; i <= length-cParallel; i+=cParallel)
 	{
@@ -15,6 +25,7 @@ void float2half(float* src, short* dst, int length) {
 		__m128i half_vector = _mm256_cvtps_ph(float_vector, 0);
 		*(__m128i*)(dst + i) = half_vector;
 	}
+#endif
 
 }
 
@@ -28,6 +39,60 @@ void multiply_float(unsigned char* src, float* gain, unsigned char* dst, unsigne
 
 void multiply(unsigned char* src, short* gain, unsigned char* dst, unsigned int cSize)
 {
+#if defined(__arm__) || defined(_M_ARM)
+	for (int x = 0;x <= cSize - 16;x += 16)
+	{
+		// load 128bits
+		uint8x16_t srcInteger = vld1q_u8(src+x);
+		// load 64bits
+		float16x4_t gainHalfLowLow   = *(float16x4_t*)(gain + x     );
+		float16x4_t gainHalfLowHigh  = *(float16x4_t*)(gain + x + 4 );
+		float16x4_t gainHalfHighLow  = *(float16x4_t*)(gain + x + 8 );
+		float16x4_t gainHalfHighHigh = *(float16x4_t*)(gain + x + 12);
+
+		// uchar -> ushort
+		uint16x8_t srcIntegerLow  = vmovl_u8(vget_low_u8 (srcInteger));
+		uint16x8_t srcIntegerHigh = vmovl_u8(vget_high_u8(srcInteger));
+		
+		// ushort -> uint
+		uint32x4_t srcIntegerLowLow   = vmovl_u16(vget_low_s16 (srcIntegerLow ));
+		uint32x4_t srcIntegerLowHigh  = vmovl_u16(vget_high_s16(srcIntegerLow ));
+		uint32x4_t srcIntegerHighLow  = vmovl_u16(vget_low_s16( srcIntegerHigh));
+		uint32x4_t srcIntegerHighHigh = vmovl_u16(vget_high_s16(srcIntegerHigh));
+
+		// half -> float
+		float32x4_t gainFloatLowLow   = vcvt_f32_f16(gainHalfLowLow  );
+		float32x4_t gainFloatLowHigh  = vcvt_f32_f16(gainHalfLowHigh );
+		float32x4_t gainFloatHighLow  = vcvt_f32_f16(gainHalfHighLow );
+		float32x4_t gainFloatHighHigh = vcvt_f32_f16(gainHalfHighHigh);
+
+		// uint -> float
+		float32x4_t srcFloatLowLow   = vcvtq_f32_u32(srcIntegerLowLow  );
+		float32x4_t srcFloatLowHigh  = vcvtq_f32_u32(srcIntegerLowHigh );
+		float32x4_t srcFloatHighLow  = vcvtq_f32_u32(srcIntegerHighLow );
+		float32x4_t srcFloatHighHigh = vcvtq_f32_u32(srcIntegerHighHigh);
+
+		// float * float (per elment)
+		float32x4_t dstFloatLowLow   = vmulq_f32(srcFloatLowLow,   gainFloatLowLow  );
+		float32x4_t dstFloatLowHigh  = vmulq_f32(srcFloatLowHigh,  gainFloatLowHigh );
+		float32x4_t dstFloatHighLow  = vmulq_f32(srcFloatHighLow,  gainFloatHighLow );
+		float32x4_t dstFloatHighHigh = vmulq_f32(srcFloatHighHigh, gainFloatHighHigh);
+
+		// float -> uint
+		uint32x4_t dstIntegerLowLow   = vcvtq_u32_f32(dstFloatLowLow  );
+		uint32x4_t dstIntegerLowHigh  = vcvtq_u32_f32(dstFloatLowHigh );
+		uint32x4_t dstIntegerHighLow  = vcvtq_u32_f32(dstFloatHighLow );
+		uint32x4_t dstIntegerHighHigh = vcvtq_u32_f32(dstFloatHighHigh);
+
+		// uint -> ushort
+		uint16x8_t dstIntegerLow  = vcombine_u16(vmovn_u16(dstIntegerLowLow),  vmovn_u16(dstIntegerLowHigh));
+		uint16x8_t dstIntegerHigh = vcombine_u16(vmovn_u16(dstIntegerHighLow), vmovn_u16(dstIntegerHighHigh));
+
+		// ushort -> uchar
+		uint8x16_t dstInteger = vcombine_u16(vmovn_u16(dstIntegerLow), vmovn_u16(dstIntegerHigh));
+		vst1q_u8(dst+x, dstInteger);
+	}
+#elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
 	__m128i v_zero = _mm_setzero_si128();
 	for (int x = 0;x <= cSize - 4;x += 4)
 	{
@@ -67,4 +132,5 @@ void multiply(unsigned char* src, short* gain, unsigned char* dst, unsigned int 
 		_mm_storel_epi64((__m128i *)(dst + x), dst_integer);
 
 	}
+#endif
 }

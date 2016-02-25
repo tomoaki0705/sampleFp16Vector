94b6dc4413136a55b03e8cf0c77330c63ccce657
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-02-25 18:45:42 +0900

    try to make both architecture similar   * reduce paralell from 16 to 8 on ARM

--- floatMul.cpp
+++ floatMul.cpp
@@ -40,57 +40,43 @@ void multiply_float(unsigned char* src, float* gain, unsigned char* dst, unsigne
 void multiply(unsigned char* src, short* gain, unsigned char* dst, unsigned int cSize)
 {
 #if defined(__arm__) || defined(_M_ARM)
-	for (int x = 0;x <= cSize - 16;x += 16)
+	for (int x = 0;x <= cSize - 8;x += 8)
 	{
-		// load 128bits
-		uint8x16_t srcInteger = vld1q_u8(src+x);
 		// load 64bits
-		float16x4_t gainHalfLowLow   = *(float16x4_t*)(gain + x     );
-		float16x4_t gainHalfLowHigh  = *(float16x4_t*)(gain + x + 4 );
-		float16x4_t gainHalfHighLow  = *(float16x4_t*)(gain + x + 8 );
-		float16x4_t gainHalfHighHigh = *(float16x4_t*)(gain + x + 12);
+		uint8x8_t srcInteger = vld1_u8(src+x);
+		// load 32bits
+		float16x4_t gainHalfLow  = *(float16x4_t*)(gain + x     );
+		float16x4_t gainHalfHigh = *(float16x4_t*)(gain + x + 4 );
 
 		// uchar -> ushort
-		uint16x8_t srcIntegerLow  = vmovl_u8(vget_low_u8 (srcInteger));
-		uint16x8_t srcIntegerHigh = vmovl_u8(vget_high_u8(srcInteger));
+		uint16x8_t srcIntegerShort  = vmovl_u8(srcInteger);
 		
 		// ushort -> uint
-		uint32x4_t srcIntegerLowLow   = vmovl_u16(vget_low_s16 (srcIntegerLow ));
-		uint32x4_t srcIntegerLowHigh  = vmovl_u16(vget_high_s16(srcIntegerLow ));
-		uint32x4_t srcIntegerHighLow  = vmovl_u16(vget_low_s16( srcIntegerHigh));
-		uint32x4_t srcIntegerHighHigh = vmovl_u16(vget_high_s16(srcIntegerHigh));
+		uint32x4_t srcIntegerLow  = vmovl_u16(vget_low_s16 (srcIntegerShort));
+		uint32x4_t srcIntegerHigh = vmovl_u16(vget_high_s16(srcIntegerShort));
 
 		// half -> float
-		float32x4_t gainFloatLowLow   = vcvt_f32_f16(gainHalfLowLow  );
-		float32x4_t gainFloatLowHigh  = vcvt_f32_f16(gainHalfLowHigh );
-		float32x4_t gainFloatHighLow  = vcvt_f32_f16(gainHalfHighLow );
-		float32x4_t gainFloatHighHigh = vcvt_f32_f16(gainHalfHighHigh);
+		float32x4_t gainFloatLow  = vcvt_f32_f16(gainHalfLow );
+		float32x4_t gainFloatHigh = vcvt_f32_f16(gainHalfHigh);
 
 		// uint -> float
-		float32x4_t srcFloatLowLow   = vcvtq_f32_u32(srcIntegerLowLow  );
-		float32x4_t srcFloatLowHigh  = vcvtq_f32_u32(srcIntegerLowHigh );
-		float32x4_t srcFloatHighLow  = vcvtq_f32_u32(srcIntegerHighLow );
-		float32x4_t srcFloatHighHigh = vcvtq_f32_u32(srcIntegerHighHigh);
+		float32x4_t srcFloatLow  = vcvtq_f32_u32(srcIntegerLow );
+		float32x4_t srcFloatHigh = vcvtq_f32_u32(srcIntegerHigh);
 
 		// float * float (per elment)
-		float32x4_t dstFloatLowLow   = vmulq_f32(srcFloatLowLow,   gainFloatLowLow  );
-		float32x4_t dstFloatLowHigh  = vmulq_f32(srcFloatLowHigh,  gainFloatLowHigh );
-		float32x4_t dstFloatHighLow  = vmulq_f32(srcFloatHighLow,  gainFloatHighLow );
-		float32x4_t dstFloatHighHigh = vmulq_f32(srcFloatHighHigh, gainFloatHighHigh);
+		float32x4_t dstFloatLow  = vmulq_f32(srcFloatLow,  gainFloatLow );
+		float32x4_t dstFloatHigh = vmulq_f32(srcFloatHigh, gainFloatHigh);
 
 		// float -> uint
-		uint32x4_t dstIntegerLowLow   = vcvtq_u32_f32(dstFloatLowLow  );
-		uint32x4_t dstIntegerLowHigh  = vcvtq_u32_f32(dstFloatLowHigh );
-		uint32x4_t dstIntegerHighLow  = vcvtq_u32_f32(dstFloatHighLow );
-		uint32x4_t dstIntegerHighHigh = vcvtq_u32_f32(dstFloatHighHigh);
+		uint32x4_t dstIntegerLow  = vcvtq_u32_f32(dstFloatLow );
+		uint32x4_t dstIntegerHigh = vcvtq_u32_f32(dstFloatHigh);
 
 		// uint -> ushort
-		uint16x8_t dstIntegerLow  = vcombine_u16(vmovn_u16(dstIntegerLowLow),  vmovn_u16(dstIntegerLowHigh));
-		uint16x8_t dstIntegerHigh = vcombine_u16(vmovn_u16(dstIntegerHighLow), vmovn_u16(dstIntegerHighHigh));
+		uint16x8_t dstIntegerShort = vcombine_u16(vmovn_u16(dstIntegerLow),  vmovn_u16(dstIntegerHigh));
 
 		// ushort -> uchar
-		uint8x16_t dstInteger = vcombine_u16(vmovn_u16(dstIntegerLow), vmovn_u16(dstIntegerHigh));
-		vst1q_u8(dst+x, dstInteger);
+		uint8x8_t dstInteger = vmovn_u16(dstIntegerShort);
+		vst1_u8(dst+x, dstInteger);
 	}
 #elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
 	__m128i v_zero = _mm_setzero_si128();

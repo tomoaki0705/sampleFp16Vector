94637b4ca72fe930faa4de89c35751d03bb70dc5
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-02-27 05:18:07 +0900

    make it more easy to read   * hide the difference of architecture

--- floatMul.cpp
+++ floatMul.cpp
@@ -1,9 +1,4 @@
 
-#if defined(__arm__) || defined(_M_ARM)
-#include <arm_neon.h>
-#elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
-#include <immintrin.h>
-#endif
 #include "floatMul.h"
 #include "simdUtils.h"
 
@@ -27,90 +22,45 @@ void multiply_float(unsigned char* src, float* gain, unsigned char* dst, unsigne
 
 void multiply(unsigned char* src, short* gain, unsigned char* dst, unsigned int cSize)
 {
-#if defined(__arm__) || defined(_M_ARM)
-	for (int x = 0;x <= cSize - 8;x += 8)
+	const unsigned int cParallel = 8;
+	for (int x = 0;x <= cSize - cParallel;x += cParallel)
 	{
 		// load 64bits
-		uint8x8_t srcInteger = vld1_u8(src+x);
+		uchar8 srcInteger = load_uchar8(src+x);
 		// load 32bits
-		float16x4_t gainHalfLow  = *(float16x4_t*)(gain + x     );
-		float16x4_t gainHalfHigh = *(float16x4_t*)(gain + x + 4 );
+		half4 gainHalfLow  = load_half4(gain + x     );
+		half4 gainHalfHigh = load_half4(gain + x + 4 );
 
 		// uchar -> ushort
-		uint16x8_t srcIntegerShort  = vmovl_u8(srcInteger);
-		
-		// ushort -> uint
-		uint32x4_t srcIntegerLow  = vmovl_u16(vget_low_s16 (srcIntegerShort));
-		uint32x4_t srcIntegerHigh = vmovl_u16(vget_high_s16(srcIntegerShort));
-
-		// uint -> float
-		float32x4_t srcFloatLow  = vcvtq_f32_u32(srcIntegerLow );
-		float32x4_t srcFloatHigh = vcvtq_f32_u32(srcIntegerHigh);
-
-		// half -> float
-		float32x4_t gainFloatLow  = vcvt_f32_f16(gainHalfLow );
-		float32x4_t gainFloatHigh = vcvt_f32_f16(gainHalfHigh);
-
-		// float * float (per elment)
-		float32x4_t dstFloatLow  = vmulq_f32(srcFloatLow,  gainFloatLow );
-		float32x4_t dstFloatHigh = vmulq_f32(srcFloatHigh, gainFloatHigh);
-
-		// float -> uint
-		uint32x4_t dstIntegerLow  = vcvtq_u32_f32(dstFloatLow );
-		uint32x4_t dstIntegerHigh = vcvtq_u32_f32(dstFloatHigh);
-
-		// uint -> ushort
-		uint16x8_t dstIntegerShort = vcombine_u16(vmovn_u16(dstIntegerLow),  vmovn_u16(dstIntegerHigh));
-
-		// ushort -> uchar
-		uint8x8_t dstInteger = vmovn_u16(dstIntegerShort);
-
-		// store
-		vst1_u8(dst+x, dstInteger);
-	}
-#elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
-	__m128i v_zero = _mm_setzero_si128();
-	for (int x = 0;x <= cSize - 8;x += 8)
-	{
-		// _mm_loadl_epi64
-		// load lower 64bit and 
-		// fill higher 64bit as 0
-		__m128i srcInteger   = _mm_loadl_epi64((__m128i const *)(src + x));
-		__m128i gainHalfLow  = _mm_loadl_epi64((__m128i const *)(gain + x    ));
-		__m128i gainHalfHigh = _mm_loadl_epi64((__m128i const *)(gain + x + 4));
-
-		// uchar -> ushort
-		__m128i srcIntegerShort = _mm_unpacklo_epi8(srcInteger, v_zero);
+		ushort8 srcIntegerShort = convert_uchar8_ushort8(srcInteger);
 
 		// ushort -> uint
-		__m128i srcIntegerLow  = _mm_unpacklo_epi16(srcIntegerShort, v_zero);
-		__m128i srcIntegerHigh = _mm_unpackhi_epi16(srcIntegerShort, v_zero);
+		uint4 srcIntegerLow  = convert_ushort8_lo_uint4(srcIntegerShort);
+		uint4 srcIntegerHigh = convert_ushort8_hi_uint4(srcIntegerShort);
 
 		// uint -> float
-		__m128 gainFloatLow  = _mm_cvtph_ps(gainHalfLow );
-		__m128 gainFloatHigh = _mm_cvtph_ps(gainHalfHigh);
+		float4 srcFloatLow  = convert_uint4_float4(srcIntegerLow );
+		float4 srcFloatHigh = convert_uint4_float4(srcIntegerHigh);
 
 		// half -> float
-		__m128 srcFloatLow  = _mm_cvtepi32_ps(srcIntegerLow );
-		__m128 srcFloatHigh = _mm_cvtepi32_ps(srcIntegerHigh);
+		float4 gainFloatLow  = convert_half4_float4(gainHalfLow );
+		float4 gainFloatHigh = convert_half4_float4(gainHalfHigh);
 
 		// float * float (per elment)
-		__m128 dstFloatLow  = _mm_mul_ps(srcFloatLow , gainFloatLow );
-		__m128 dstFloatHigh = _mm_mul_ps(srcFloatHigh, gainFloatHigh);
+		float4 dstFloatLow  = srcFloatLow  * gainFloatLow;
+		float4 dstFloatHigh = srcFloatHigh * gainFloatHigh;
 
 		// float -> uint
-		__m128i dstIntegerLow  = _mm_cvtps_epi32(dstFloatLow );
-		__m128i dstIntegerHigh = _mm_cvtps_epi32(dstFloatHigh);
+		uint4 dstIntegerLow  = convert_float4_uint4(dstFloatLow );
+		uint4 dstIntegerHigh = convert_float4_uint4(dstFloatHigh);
 
 		// uint -> ushort
-		__m128i dstIntegerShort = _mm_packs_epi32(dstIntegerLow, dstIntegerHigh);
+		ushort8 dstIntegerShort = convert_uint4_ushort8(dstIntegerLow, dstIntegerHigh);
 
 		// ushort -> uchar
-		__m128i dstInteger = _mm_packus_epi16(dstIntegerShort, v_zero);
+		uchar8 dstInteger = convert_ushort8_uchar8(dstIntegerShort);
 
 		// store
-		_mm_storel_epi64((__m128i *)(dst + x), dstInteger);
-
+		store_uchar8(dst + x, dstInteger);
 	}
-#endif
 }
--- simdUtils.h
+++ simdUtils.h
@@ -3,14 +3,46 @@
 #include <arm_neon.h>
 typedef float32x4_t float4;
 typedef float16x4_t half4;
+typedef uint8x8_t uchar8;
+typedef uint16x8_t ushort8;
+typedef uint32x4_t uint4; 
 inline float4 load_float4(const float* ptr) { return *(float4*)ptr; }
+inline half4 load_half4(const short* ptr)   { return *(float16x4_t*)ptr; }
+inline uchar8 load_uchar8(const unsigned char* ptr) { return vld1_u8(ptr); }
 inline void store_half4(void* ptr, half4 h) { *(half4*)ptr = h; }
+inline void store_uchar8(void* ptr, uchar8 c)   { vst1_u8(ptr, c); }
 inline half4 convert_float4_half4(float4 f) { return vcvt_f16_f32(f); }
+inline ushort8 convert_uchar8_ushort8(uchar8 c) { return vmovl_u8(c); }
+inline uint4 convert_ushort8_lo_uint4(ushort8 s){ return vmovl_u16(vget_low_s16 (s); }
+inline uint4 convert_ushort8_hi_uint4(ushort8 s){ return vmovl_u16(vget_high_s16(s); }
+inline uchar8 convert_ushort8_uchar8(ushort8 s) { return vmovn_u16(s); }
+inline float4 convert_uint4_float4(uint4 i) { return vcvtq_f32_u32(i); }
+inline float4 convert_half4_float4(half4 h) { return vcvt_f32_f16(h); }
+inline uint4 convert_float4_uint4(float4 f) { return vcvtq_u32_f32(f); }
+inline ushort8 convert_uint4_ushort8(uint4 a, uint4 b){ return vcombine_u16(vmovn_u16(a), vmovn_u16(b)); }
+inline float4 multiply_float4(float4 a, float4 b)     { return vmulq_f32(a, b); }
 #elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
 #include <immintrin.h>
 typedef __m128 float4;
 typedef __m128i half4;
+typedef __m128i uchar8;
+typedef __m128i ushort8;
+typedef __m128i uint4; 
 inline float4 load_float4(const float* ptr) { return _mm_load_ps(ptr); }
+inline half4 load_half4(const short* ptr)   { return _mm_loadl_epi64((const half4*)ptr); }
+inline uchar8 load_uchar8(const unsigned char* ptr) { return _mm_loadl_epi64((const uchar8*)ptr); }
 inline void store_half4(void* ptr, half4 h) { _mm_storel_epi64((half4*)ptr, h); }
+inline void store_uchar8(void* ptr, uchar8 c)   { _mm_storel_epi64((uchar8*)ptr, c); }
 inline half4 convert_float4_half4(float4 f) { return _mm_cvtps_ph(f, 0); }
+inline ushort8 convert_uchar8_ushort8(uchar8 c) { return _mm_cvtepu8_epi16(c); }
+inline uint4 convert_ushort8_lo_uint4(ushort8 s){ __m128i v_zero = _mm_setzero_si128(); return _mm_unpacklo_epi16(s, v_zero); }
+inline uint4 convert_ushort8_hi_uint4(ushort8 s){ __m128i v_zero = _mm_setzero_si128(); return _mm_unpackhi_epi16(s, v_zero); }
+inline uchar8 convert_ushort8_uchar8(ushort8 s) { __m128i v_zero = _mm_setzero_si128(); return _mm_packus_epi16(s, v_zero); }
+inline float4 convert_uint4_float4(uint4 i) { return _mm_cvtepi32_ps(i); }
+inline float4 convert_half4_float4(half4 h) { return _mm_cvtph_ps(h); }
+inline uint4 convert_float4_uint4(float4 f) { return _mm_cvtps_epi32(f); }
+inline ushort8 convert_uint4_ushort8(uint4 a, uint4 b){ return _mm_packs_epi32(a, b); }
+inline float4 multiply_float4(float4 a, float4 b)     { return _mm_mul_ps(a, b); }
 #endif
+
+

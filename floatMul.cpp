6279d7d2be03f331f03c505e79d4bed0f535c068
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-02-26 18:32:58 +0900

    try to make both architecture similar   * increase parallel from 4 to 8 on x86

--- floatMul.cpp
+++ floatMul.cpp
@@ -82,42 +82,46 @@ void multiply(unsigned char* src, short* gain, unsigned char* dst, unsigned int 
 	}
 #elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
 	__m128i v_zero = _mm_setzero_si128();
-	for (int x = 0;x <= cSize - 4;x += 4)
+	for (int x = 0;x <= cSize - 8;x += 8)
 	{
 		// _mm_loadl_epi64
 		// load lower 64bit and 
 		// fill higher 64bit as 0
-		__m128i src_integer = _mm_loadl_epi64((__m128i const *)(src + x));
-		__m128i gain_half = _mm_loadl_epi64((__m128i const *)(gain + x));
+		__m128i srcInteger   = _mm_loadl_epi64((__m128i const *)(src + x));
+		__m128i gainHalfLow  = _mm_loadl_epi64((__m128i const *)(gain + x    ));
+		__m128i gainHalfHigh = _mm_loadl_epi64((__m128i const *)(gain + x + 4));
 
 		// uchar -> ushort
-		src_integer = _mm_unpacklo_epi8(src_integer, v_zero);
+		__m128i srcIntegerShort = _mm_unpacklo_epi8(srcInteger, v_zero);
 
 		// ushort -> uint
-		src_integer = _mm_unpacklo_epi16(src_integer, v_zero);
-
-		// half -> float
-		__m128 src_float   = _mm_cvtepi32_ps(src_integer);
+		__m128i srcIntegerLow  = _mm_unpacklo_epi16(srcIntegerShort, v_zero);
+		__m128i srcIntegerHigh = _mm_unpackhi_epi16(srcIntegerShort, v_zero);
 
 		// uint -> float
-		__m128 gain_float = _mm_cvtph_ps(gain_half);
+		__m128 gainFloatLow  = _mm_cvtph_ps(gainHalfLow );
+		__m128 gainFloatHigh = _mm_cvtph_ps(gainHalfHigh);
+
+		// half -> float
+		__m128 srcFloatLow  = _mm_cvtepi32_ps(srcIntegerLow );
+		__m128 srcFloatHigh = _mm_cvtepi32_ps(srcIntegerHigh);
 
 		// float * float (per elment)
-		__m128 dst_float = _mm_mul_ps(src_float, gain_float);
-
-		//__m256 _mm256_mul_ps (__m256 a, __m256 b)
+		__m128 dstFloatLow  = _mm_mul_ps(srcFloatLow , gainFloatLow );
+		__m128 dstFloatHigh = _mm_mul_ps(srcFloatHigh, gainFloatHigh);
 
 		// float -> uint
-		__m128i dst_integer = _mm_cvtps_epi32(dst_float);
+		__m128i dstIntegerLow  = _mm_cvtps_epi32(dstFloatLow );
+		__m128i dstIntegerHigh = _mm_cvtps_epi32(dstFloatHigh);
 
 		// uint -> ushort
-		dst_integer = _mm_packs_epi32(dst_integer, v_zero);
+		__m128i dstIntegerShort = _mm_packs_epi32(dstIntegerLow, dstIntegerHigh);
 
 		// ushort -> uchar
-		dst_integer = _mm_packus_epi16(dst_integer, v_zero);
+		__m128i dstInteger = _mm_packus_epi16(dstIntegerShort, v_zero);
 
-		// sotre
-		_mm_storel_epi64((__m128i *)(dst + x), dst_integer);
+		// store
+		_mm_storel_epi64((__m128i *)(dst + x), dstInteger);
 
 	}
 #endif

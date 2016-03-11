59517306fcb656412afdcc86e3e65742d7755899
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-03-11 18:50:52 +0900

    run the demo   * import pre-converted float/ half image   * multiply using half/float conversion   * copy the image files to the binary directory

--- demoMain.cpp
+++ demoMain.cpp
@@ -8,6 +8,50 @@
 
 cv::VideoCapture capture;
 const char windowName[]  = "demo";
+const char* imagePath[][2] = {
+	{ "defaultMaskHalf.png", "defaultMaskFloat.png"},
+	{ "lenaMaskHalf.png",    "lenaMaskFloat.png"},
+};
+
+enum precision
+{
+	percisionHalf,
+	precisionFloat,
+};
+enum device
+{
+	useCpuSimd,
+	useGpu,
+};
+
+void multiplyImage(cv::Mat &image, cv::Mat gain)
+{
+	cv::Mat stub, b, g, r;
+	std::vector<cv::Mat> arrayColor;
+	arrayColor.push_back(b);
+	arrayColor.push_back(g);
+	arrayColor.push_back(r);
+	cv::split(image, arrayColor);
+
+	switch (gain.elemSize())
+	{
+	case 2:
+		for (unsigned int i = 0; i < arrayColor.size(); i++)
+		{
+			multiply(arrayColor[i].data, (short*)gain.data, arrayColor[i].data, arrayColor[i].rows*arrayColor[i].cols);
+		}
+		break;
+	case 4:
+	default:
+		for (unsigned int i = 0; i < arrayColor.size(); i++)
+		{
+			arrayColor[i].convertTo(stub, CV_32FC1);
+			multiply(stub, gain, arrayColor[i], 1.0, CV_8UC1);
+		}
+		break;
+	}
+	cv::merge(arrayColor, image);
+}
 
 bool isFinish(char key)
 {
@@ -24,7 +68,6 @@ bool isFinish(char key)
 	return finishKey;
 }
 
-
 int main(int argc, char**argv)
 {
 	capture.open(0);
@@ -35,9 +78,39 @@ int main(int argc, char**argv)
 	cv::imshow(windowName, image);
 
 	char key = -1;
+	enum device statusDevice = useCpuSimd;
+	cv::Mat stub = cv::imread(imagePath[0][0], cv::IMREAD_UNCHANGED);
+	cv::Mat gain = cv::Mat(stub.rows, stub.cols, CV_16SC1, stub.data);
 	while (isFinish(key) == false)
 	{
 		capture >> image;
+
+		switch (key)
+		{
+		case 'h':
+		case 'H':
+			stub = cv::imread(imagePath[0][0], cv::IMREAD_UNCHANGED);
+			gain = cv::Mat(stub.rows, stub.cols, CV_16SC1, stub.data);
+			break;
+		case 'f':
+		case 'F':
+			stub = cv::imread(imagePath[0][1], cv::IMREAD_UNCHANGED);
+			gain = cv::Mat(stub.rows, stub.cols, CV_32FC1, stub.data);
+			break;
+		default:
+			break;
+		}
+
+		if (statusDevice == useCpuSimd)
+		{
+			multiplyImage(image, gain);
+		}
+		else
+		{
+			// CUDA
+			// empty for now
+		}
+
 		cv::imshow(windowName, image);
 		key = cv::waitKey(1);
 	}

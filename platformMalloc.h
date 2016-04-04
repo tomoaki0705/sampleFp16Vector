2750eb29455344ddf09008bb6d356dc2150c3c92
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-04-04 22:30:06 +0900

    clean up code   * align the code

--- platformMalloc.h
+++ platformMalloc.h
@@ -1,11 +1,11 @@
 #include <malloc.h>
 #ifdef _MSC_VER
 #include <cstdlib>
-#define alignedMalloc(size,align)	_aligned_malloc(size,align)
-#define alignedFree(ptr)			_aligned_free(ptr)
+inline void* alignedMalloc(size_t size,size_t align) { return _aligned_malloc(size,align); }
+#define alignedFree(ptr)			                 _aligned_free(ptr)
 #elif defined (__GNUC__)
 #include <stdlib.h>
 inline void* alignedMalloc(size_t size,size_t align) { void* a; volatile int b = posix_memalign(&a,align,size); return a; }
-#define alignedFree(ptr)			free(ptr)
+#define alignedFree(ptr)			                 free(ptr)
 #endif
 
